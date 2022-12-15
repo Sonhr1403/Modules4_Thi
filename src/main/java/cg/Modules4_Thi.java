af404c4774492e41a3ca53502ad271e6dd5dcbe9
Sonhr1403 <sonhr1403@gmail.com>
2022-12-15 11:56:50 +0700

    Initial commit

---
+++ src/main/java/cg/Modules4_Thi.java
@@ -0,0 +1,13 @@
+package cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modules4_Thi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modules4_Thi.class, args);
+    }
+
+}
